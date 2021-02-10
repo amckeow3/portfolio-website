28476b059e1256927b87aa189b0ff065d81e2bc7
adriannamckeown <71573922+adriannamckeown@users.noreply.github.com>
2021-02-09 20:27:55 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from '././header/header.component';
+import { ResumeComponent } from './resume/resume.component';
+import {WorkComponent } from './work/work.component';
+import { BlogComponent } from './blog/blog.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', 
+    component: HomepageComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'work',
+    component: WorkComponent
+  },
+  {
+    path: 'blog',
+    component: BlogComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { WorkComponent } from './work/work.component';
+import { ResumeComponent } from './resume/resume.component';
+import { MenuComponent } from './menu/menu.component';
+import { BlogComponent } from './blog/blog.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    WorkComponent,
+    ResumeComponent,
+    MenuComponent,
+    BlogComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
